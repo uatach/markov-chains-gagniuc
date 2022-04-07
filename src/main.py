17c2ebda83954a459dddc1170a970e2680f94747
edson duarte <uatach451@gmail.com>
2022-04-07 18:34:18 -0300

    apply code style

--- src/main.py
+++ src/main.py
@@ -10,27 +10,26 @@ def algo01():
         return jar[choice]
 
     jars = [
-        'WWBBBBBBBB',
-        'WWWWWBBBBB',
+        "WWBBBBBBBB",
+        "WWWWWBBBBB",
     ]
 
     draws = 17
     choice = random.randrange(len(jars))
     ball = draw(jars[choice])
 
-    output = f' Jar W[{ball}],'
+    output = f" Jar W[{ball}],"
 
     for _ in range(draws):
-        if ball == 'W':
+        if ball == "W":
             ball = draw(jars[0])
-            output += f' Jar W[{ball}],'
+            output += f" Jar W[{ball}],"
         else:
             ball = draw(jars[1])
-            output += f' Jar B[{ball}],'
+            output += f" Jar B[{ball}],"
 
     print(output)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     algo01()
-
