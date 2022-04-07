d4ee4dba999210ccea1c4847936bd03f4547da2e
edson duarte <edsonduarte1990@gmail.com>
2022-04-07 19:28:45 -0300

    add algorithm 2

--- src/main.py
+++ src/main.py
@@ -4,29 +4,48 @@ import random
 random.seed(42)
 
 
-def algo01():
-    def draw(jar):
-        choice = random.randrange(len(jar))
-        return jar[choice]
+def draw01(jar):
+    choice = random.randrange(len(jar))
+    return jar[choice]
 
+
+def simulate01(jars):
+    draws = 17
+    choice = random.choice(list(jars.keys()))
+    ball = draw01(jars[choice])
+    output = f" Jar {choice}[{ball}],"
+
+    for _ in range(draws):
+        choice = ball
+        ball = draw01(jars[choice])
+        output += f" Jar {choice}[{ball}],"
+    return output
+
+
+def algo01():
     jars = {
         "W": "WWBBBBBBBB",
         "B": "WWWWWBBBBB",
     }
 
-    draws = 17
-    choice = random.choice(list(jars.keys()))
-    ball = draw(jars[choice])
+    print(simulate01(jars))
 
-    output = f" Jar {choice}[{ball}],"
 
-    for _ in range(draws):
-        choice = ball
-        ball = draw(jars[choice])
-        output += f" Jar {choice}[{ball}],"
+def fill(ratio):
+    white = int(100 * ratio)
+    black = 100 - white
+    assert white + black == 100
+    return white * "W" + black * "B"
 
-    print(output)
+
+def algo02():
+    jars = {
+        "W": fill(0.2),
+        "B": fill(0.6),
+    }
+
+    print(simulate01(jars))
 
 
 if __name__ == "__main__":
-    algo01()
+    algo02()
