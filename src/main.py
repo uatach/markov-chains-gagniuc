a02624800f2921e1999487c4d65bf89a7c40b515
edson duarte <uatach451@gmail.com>
2022-04-07 18:32:44 -0300

    add algorithm 1

---
+++ src/main.py
@@ -0,0 +1,36 @@
+import random
+
+
+random.seed(42)
+
+
+def algo01():
+    def draw(jar):
+        choice = random.randrange(len(jar))
+        return jar[choice]
+
+    jars = [
+        'WWBBBBBBBB',
+        'WWWWWBBBBB',
+    ]
+
+    draws = 17
+    choice = random.randrange(len(jars))
+    ball = draw(jars[choice])
+
+    output = f' Jar W[{ball}],'
+
+    for _ in range(draws):
+        if ball == 'W':
+            ball = draw(jars[0])
+            output += f' Jar W[{ball}],'
+        else:
+            ball = draw(jars[1])
+            output += f' Jar B[{ball}],'
+
+    print(output)
+
+
+if __name__ == '__main__':
+    algo01()
+
