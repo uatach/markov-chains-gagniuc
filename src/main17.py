13a035692a4df42b067d899bf2abe6b4c8b5c8a2
edson duarte <edsonduarte1990@gmail.com>
2022-04-12 21:34:15 -0300

    add algorithm 17

---
+++ src/main17.py
@@ -0,0 +1,58 @@
+from main01 import draw
+from main14 import fill
+
+
+configs = {
+    "A": {
+        "A": 0,
+        "B": 1,
+        "C": 0,
+        "D": 0,
+    },
+    "B": {
+        "A": 1 / 3,
+        "B": 0,
+        "C": 1 / 3,
+        "D": 1 / 3,
+    },
+    "C": {
+        "A": 0,
+        "B": 1,
+        "C": 0,
+        "D": 0,
+    },
+    "D": {
+        "A": 0,
+        "B": 0,
+        "C": 1,
+        "D": 0,
+    },
+}
+
+
+def simulate(jars):
+    draws = 10000
+
+    choice = "B"
+    ball = draw(jars["A"])
+    output = f"{ball}"
+
+    for _ in range(draws):
+        choice = ball
+        ball = draw(jars[choice])
+        output += f"{ball}"
+    return output
+
+
+def main():
+    jars = {k: fill(v, 100) for k, v in configs.items()}
+    output = simulate(jars)
+    size = len(output)
+
+    for k in configs.keys():
+        fraction = int(100 / size * output.count(k))
+        print(f"{k}: {fraction:3d}%")
+
+
+if __name__ == "__main__":
+    main()
